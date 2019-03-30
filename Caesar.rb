0c3c425ba86cbba69c3c65dc7f3db01bb5a2008d
Kovali0 <kowal.bartek.10@gmail.com>
2019-03-30 21:15:59 +0100

    [Add][Change] Small changes in menu and add decryption method for Caesar

--- Caesar.rb
+++ Caesar.rb
@@ -26,4 +26,32 @@ class Caesar
         puts 'Your message was encrypted.'
         return crypto_message
     end
+
+    def decryption_Caesar (plaintext)
+        puts 'Enter the decryption key.'
+        ceaserkey = gets.chomp.to_i
+        text_array = plaintext.codepoints.to_a
+        ln = text_array.length
+        cipher_text = Array.new(ln)
+        i = 0
+        text_array.each do |c|
+            case c
+                when 'a'
+                    c = 'z'
+                when 'A'
+                    c = 'Z'
+                when ' '
+                    c = ' ' #= '_' - optional
+                when '.'
+                    c = '.'
+                else
+                    c -= ceaserkey
+                end
+            cipher_text[i] = c.chr
+            i += 1
+        end
+        explicit_message = cipher_text.join('')
+        puts 'Decryption was successful.'
+        return explicit_message
+    end
 end
--- EncryptionProgram.rb
+++ EncryptionProgram.rb
@@ -3,45 +3,99 @@ class Program
     require_relative 'AffineCipher'
     require_relative 'PBCipher'
 
-    puts 'Choose the encryption method from below:'
-    puts '1-Caesar cipher'
-    puts '2-Affine cipher'
-    puts '3-Permutational block cipher'
+    w = 1
+    while w==1 do
 
-    cipher_id = gets.chomp
+        puts 'Welcom. What do you want to do?'
+        puts '1-Encryption'
+        puts '2-Decryption'
 
-    war = 1;
-    while war==1 do
-        puts 'Enter the file name which do you want to encrypt. (Remember that file have to have .txt extension)'
-        filename = gets.chomp
-        if filename.end_with?(".txt") && File.file?(filename)
-            war = 0
+        operation = gets.chomp
+
+        if operation == '1'
+
+            puts 'Choose the encryption method from below:'
+            puts '1-Caesar cipher'
+            puts '2-Affine cipher'
+            puts '3-Permutational block cipher'
+
+            cipher_id = gets.chomp
+
+            war = 1;
+            while war==1 do
+                puts 'Enter the file name which do you want to encrypt. (Remember that file have to have .txt extension)'
+                filename = gets.chomp
+                if filename.end_with?(".txt") && File.file?(filename)
+                    war = 0
+                else
+                    puts 'Your file doesn t exist or you made mistake in entered filename. Try again.'
+                end
+            end
+
+            file = File.open(filename, "rb")
+            plaintext = file.read
+
+            case cipher_id
+                when '1'
+                    @caesar = Caesar.new()
+                    content = @caesar.encrypt_by_Caesar(plaintext)
+                when '2'
+                    @ac = AffineCipher.new()
+                    content = @ac.affine_cipher(plaintext)
+                when '3'
+                    @pbc = PBCipher.new()
+                    content  = @pbc.permutational_block_cipher(plaintext)
+                begin
+                    rescue => exception
+                end
+                else
+                    p 'Błąd wyboru'
+            end
+
+            filename = "encrypted" + filename
+            file = File.open(filename, "a") 
+            file.puts content
+            w = 0
+
+        elsif operation == '2'
+
+            puts 'Choose the cipher to decryption from below:'
+            puts '1-Caesar cipher'
+
+            cipher_id = gets.chomp
+
+            war = 1;
+            while war==1 do
+                puts 'Enter the file name which do you want to decrypt. (Remember that file have to have .txt extension)'
+                filename = gets.chomp
+                if filename.end_with?(".txt") && File.file?(filename)
+                    war = 0
+                else
+                    puts 'Your file doesn t exist or you made mistake in entered filename. Try again.'
+                end
+            end
+
+            file = File.open(filename, "rb")
+            plaintext = file.read
+
+            case cipher_id
+                when '1'
+                    @caesar = Caesar.new()
+                    content = @caesar.decryption_Caesar(plaintext)
+                begin
+                    rescue => exception
+                end
+                else
+                    p 'Błąd wyboru'
+            end
+
+            filename = "decrypted" + filename
+            file = File.open(filename, "a") 
+            file.puts content
+            w = 0
         else
-            puts 'Your file doesn t exist or you made mistake in entered filename. Try again.'
+            puts 'Error, pleas enter your choice again.'
+            w = 1
         end
     end
-
-    file = File.open(filename, "rb")
-    plaintext = file.read
-
-    case cipher_id
-        when '1'
-            @caesar = Caesar.new()
-            content = @caesar.encrypt_by_Caesar(plaintext)
-        when '2'
-            @ac = AffineCipher.new()
-            content = @ac.affine_cipher(plaintext)
-        when '3'
-            @pbc = PBCipher.new()
-            content  = @pbc.permutational_block_cipher(plaintext)
-        begin
-            rescue => exception
-        end
-        else
-            p 'Błąd wyboru'
-    end
-
-    filename = "encrypted" + filename
-    file = File.open(filename, "a") 
-    file.puts content
 end
